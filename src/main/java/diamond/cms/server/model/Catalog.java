1c6b9ae23a443060a3cc2d1353301173cd2bfe57
周超 <zhouchao@zhixueyun.com>
2016-11-21 09:01:00 +0800

    change catalog

--- src/main/java/diamond/cms/server/model/Catalog.java
+++ src/main/java/diamond/cms/server/model/Catalog.java
@@ -9,14 +9,4 @@ public class Catalog extends CCatalogPojo{
      */
     private static final long serialVersionUID = 2847429716821576726L;
 
-    private String parentName;
-
-    public String getParentName() {
-        return parentName;
-    }
-
-    public void setParentName(String parentName) {
-        this.parentName = parentName;
-    }
-
 }
--- src/main/java/diamond/cms/server/services/CatalogService.java
+++ src/main/java/diamond/cms/server/services/CatalogService.java
@@ -1,42 +1,37 @@
 package diamond.cms.server.services;
 
-import org.jooq.DSLContext;
-import org.jooq.Record;
-import org.jooq.SelectOnConditionStep;
 import org.springframework.stereotype.Service;
 
 import diamond.cms.server.core.PageResult;
-import diamond.cms.server.dao.Fields;
 import diamond.cms.server.model.Catalog;
-import diamond.cms.server.model.jooq.tables.CCatalog;
 
 @Service
 public class CatalogService extends GenericService<Catalog>{
 
     @Override
     public PageResult<Catalog> page(PageResult<Catalog> page) {
-        dao.fetch(page, e -> getCatalog(e), m -> getCatalogMapper(m));
+        dao.fetch(page);
         return page;
     }
 
     @Override
     public Catalog get(String id) {
-        return dao.execute(e -> getCatalog(e).where(CCatalog.C_CATALOG.ID.eq(id)).fetchOne(m -> getCatalogMapper(m)));
+        return dao.get(id);
     };
 
-    private Catalog getCatalogMapper(Record m) {
-        CCatalog cataTable = CCatalog.C_CATALOG;
-        CCatalog parent = cataTable.as("parent");
-        String parentName = m.into(parent.NAME.as("parentName")).into(String.class);
-        Catalog cata = m.into(Catalog.class);
-        cata.setParentName(parentName);
-        return cata;
-    }
-
-    private SelectOnConditionStep<?> getCatalog(DSLContext e) {
-        CCatalog cataTable = CCatalog.C_CATALOG;
-        CCatalog parent = cataTable.as("parent");
-        return e.select(Fields.all(cataTable.fields(), parent.NAME.as("parentName"))).from(cataTable)
-        .leftJoin(parent).on(parent.ID.eq(cataTable.PARENT_ID));
-    }
+//    private Catalog getCatalogMapper(Record m) {
+//        CCatalog cataTable = CCatalog.C_CATALOG;
+//        CCatalog parent = cataTable.as("parent");
+//        String parentName = m.into(parent.NAME.as("parentName")).into(String.class);
+//        Catalog cata = m.into(Catalog.class);
+//        cata.setParentName(parentName);
+//        return cata;
+//    }
+//
+//    private SelectOnConditionStep<?> getCatalog(DSLContext e) {
+//        CCatalog cataTable = CCatalog.C_CATALOG;
+//        CCatalog parent = cataTable.as("parent");
+//        return e.select(Fields.all(cataTable.fields(), parent.NAME.as("parentName"))).from(cataTable)
+//        .leftJoin(parent).on(parent.ID.eq(cataTable.PARENT_ID));
+//    }
 }
