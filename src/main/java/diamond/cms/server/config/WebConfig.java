a73f10986fb5c47b7eb34896bac03045d2e3fe38
周超 <zhouchao@zhixueyun.com>
2016-12-08 13:37:55 +0800

    update cros implement

--- src/main/java/diamond/cms/server/config/WebConfig.java
+++ src/main/java/diamond/cms/server/config/WebConfig.java
@@ -8,12 +8,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import diamond.cms.server.interceptor.AuthorizationInterceptor;
+import diamond.cms.server.interceptor.CROSInterceptor;
 import diamond.cms.server.json.spring.JsonReturnHandler;
 
 @Configuration
@@ -22,6 +22,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Resource
     AuthorizationInterceptor authorizationInterceptor;
+
+    @Resource
+    CROSInterceptor crosInterceptor;
 
     @Bean
     public JsonReturnHandler JsonReturnHandler(){
@@ -42,12 +45,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     }
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(crosInterceptor);
         registry.addInterceptor(authorizationInterceptor);
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("POST","GET","DELETE","PUT");
     }
 
 }
---
+++ src/main/java/diamond/cms/server/interceptor/CROSInterceptor.java
@@ -0,0 +1,34 @@
+package diamond.cms.server.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class CROSInterceptor implements HandlerInterceptor{
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Methods", request.getMethod());
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
+        response.setHeader("Access-Control-Allow-Credentials","true");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+    }
+
+}
