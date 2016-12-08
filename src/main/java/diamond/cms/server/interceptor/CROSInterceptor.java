233b065603b0a51d2bb476efe927ced95a928d47
周超 <zhouchao@zhixueyun.com>
2016-12-08 16:12:10 +0800

    update header cros

--- src/main/java/diamond/cms/server/interceptor/CROSInterceptor.java
+++ src/main/java/diamond/cms/server/interceptor/CROSInterceptor.java
@@ -16,7 +16,7 @@ public class CROSInterceptor implements HandlerInterceptor{
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", request.getMethod());
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization," + request.getHeader("Access-Control-Request-Headers"));
         response.setHeader("Access-Control-Allow-Credentials","true");
         return true;
     }
